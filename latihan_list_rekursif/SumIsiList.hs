67866366b9d5d49cec80782628e95c29856a7e2d
karolyangqian <karolyangqian14@gmail.com>
2024-03-03 21:43:31 +0700

    revisi SumIsiList

--- latihan_list_rekursif/SumIsiList.hs
+++ latihan_list_rekursif/SumIsiList.hs
@@ -1,5 +1,10 @@
+-- JUMLAH ISI LIST              sumIsiList l
+-- DEFINISI DAN SPESIFIKASI
 sumIsiList :: [Int] -> Int
+{-  sumIsiList l menerima list of integer lalu mengembalikan jumlah setiap 
+    elemen dalam list. -}
 
+-- REALISASI
 sumIsiList l
     | length l == 1 = head l
     | length l > 1 = head l + sumIsiList (tail l)
