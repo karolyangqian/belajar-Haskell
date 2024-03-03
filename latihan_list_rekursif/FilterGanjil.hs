dc1c6538f8d5ac73e516aee73b6bef80b0fa83e0
karolyangqian <karolyangqian14@gmail.com>
2024-03-03 21:58:01 +0700

    revisi

--- latihan_list_rekursif/FilterGanjil.hs
+++ latihan_list_rekursif/FilterGanjil.hs
@@ -1,6 +1,14 @@
+-- FILTER GANJIL                    filterGanjil li
+-- DEFINISI DAN SPESIFIKASI
 filterGanjil :: [Int] -> [Int]
+{-  filterGanjil li menerima list of integer li lalu mengembalikan list of integer
+    yang setiap elemennya hanya terdiri atas bilangan ganjil yang muncul di li 
+    dengan urutan kemunculan yang sama. -}
 
-filterGanjil li
-    | null li = []
-    | mod (head li) 2 /= 0 = head li : filterGanjil (tail li)
-    | otherwise = filterGanjil (tail li)
+-- REALISASI
+filterGanjil li = 
+    let konso e l = e : l
+    in
+        if null li then []
+        else if mod (head li) 2 /= 0 then konso (head li) (filterGanjil (tail li))
+        else filterGanjil (tail li)
--- latihan_list_rekursif/IsEqFront.hs
+++ latihan_list_rekursif/IsEqFront.hs
@@ -1,6 +1,12 @@
+-- APAKAH POTONGAN AWAL SAMA                    isEqFront t1 t2
+-- DEFINISI DAN SPESIFIKASI
 isEqFront :: [Char] -> [Char] -> Bool
+{-  isEqFront menerima 2 list of characters t1 dan t2 yang tidak kosong
+    dan mengembalikan True jika potongan awal t2 mengandung t1 dan mengembalikan
+    False jika sebaliknya. -}
 
 isEqFront t1 t2 
+    | null t2 && not (null t1)= False
     | null t1 = True
     | head t1 /= head t2 = False
     | otherwise = isEqFront (tail t1) (tail t2)
